abf6e140c13f7e53ba81d5950e04f9af4002ffa1
JLYam <aymeric.kornatowski59@gmail.com>
2026-03-31 23:58:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -63,6 +63,6 @@ app.delete("/depart/:id", async (req, res) => {
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("🚀 API READY sur port", PORT);
 });
